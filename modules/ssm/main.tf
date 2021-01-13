54376f3f11ec2133625d5ebe8d6d83b989d7b26b
Luis Henrique Zanandrea Paese <lhzpaese@gmail.com>
2021-01-12 22:26:26 -0300

    changin ssm main.tf to use jsonencode function

--- modules/ssm/main.tf
+++ modules/ssm/main.tf
@@ -6,23 +6,24 @@ resource "aws_ssm_document" "ad-join-domain" {
   name          = "ad-join-domain"
   document_type = "Command"
 
-  content = <<DOC
+  content = jsonencode(
     {
-      "schemaVersion": "2.2"
-      "description": "aws:domainJoin"
-      "mainSteps": [
+      "schemaVersion" = "2.2"
+      "description" = "aws:domainJoin"
+      "mainSteps" = [
         {
-          "action": "aws:domainJoin",
-          "name": "domainJoin",
-          "inputs": {
-            "directoryId" : ${var.ad_id},
-            "directoryName" : ${data.aws_directory_service_directory.ad_info.name},
-            "dnsIpAddresses" : ""
+          action = "aws:domainJoin"
+          name = "domainJoin"
+          inputs = {
+            "directoryId" = var.ad_id,
+            "directoryName" = data.aws_directory_service_directory.ad_info.name
+            "dnsIpAddresses" = data.aws_directory_service_directory.ad_info.dns_ip_addresses
           }
         }
       ]
     }
-  DOC
+  )
+
 }
 
 resource "aws_ssm_association" "linux_association" {
