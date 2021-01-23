68df411964d1295b6191723cbf6633212b8d665a
Luis Henrique Zanandrea Paese <lhzpaese@gmail.com>
2021-01-23 14:07:21 -0300

    adding public ip to ec2 outputs

--- modules/ec2/outputs.tf
+++ modules/ec2/outputs.tf
@@ -6,6 +6,10 @@ output "name" {
   value = aws_instance.ds-ec2.tags.Name
 }
 
+output "ec2_public_ip" {
+  value = aws_instance.ds-ec2.public_ip
+}
+
 output "ec2_public_dns" {
   value = aws_instance.ds-ec2.public_dns
 }
--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,11 @@
+output "ec2_id" {
+  value = module.ec2.id
+}
+
+output "ec2_public_ip" {
+  value = module.ec2.ec2_public_ip
+}
+
 output "ec2_public_dns" {
   value = module.ec2.ec2_public_dns
 }
